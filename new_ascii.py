babe3afd79972ab667f9960b0197a10852ac8d93
ThatKpopStanDev <thatkpopstandev@gmail.com>
2025-02-06 00:34:17 -0300

    i just deleted a line and now is more HD i hate this language

--- new_ascii.py
+++ new_ascii.py
@@ -5,48 +5,44 @@ import time
 import os
 import numpy as np
 
-
 def find(name, path):
     for root, _, files in os.walk(path):
-        name = name + ".webm"
-        if name in files:
-            return os.path.join(root, name)
-    raise ValueError
-
+        if name + ".webm" in files:
+            return os.path.join(root, name + ".webm")
+    raise ValueError("Archivo no encontrado")
 
 def resize_image(image, new_width):
     orig_width, orig_height = image.size
     ratio = orig_height / orig_width
     new_height = int(new_width * ratio)
     return image.resize((new_width, new_height))
-
-
+def adjust_brightness(r, g, b, factor=1.5):
+    # Aumentar el brillo multiplicando por un factor
+    r = min(255, int(r * factor))
+    g = min(255, int(g * factor))
+    b = min(255, int(b * factor))
+    return r, g, b
 def pixels_to_ascii_colored(image):
     image = image.convert("RGB")
     pixels = np.array(image)
     ascii_image = ""
-
     for row in pixels:
         for pixel in row:
-            if isinstance(pixel, np.ndarray) and len(pixel) == 3:
-                r, g, b = pixel
-            else:
-                r, g, b = 255, 255, 255
-
-            brightness = int((r + g + b) / 3)
-            char = ASCII_CHARS[brightness * len(ASCII_CHARS) // 256]
-
+            r, g, b = map(lambda x: max(0, min(255, int(x))), pixel)
+            char = ASCII_CHARS[len(ASCII_CHARS) // 256]
             ascii_image += f"\033[38;2;{r};{g};{b}m{char}\033[0m"
         ascii_image += "\n"
-
     return ascii_image
-
 
 def image_to_ascii(frame):
     frame = resize_image(frame, new_width=width)
     return pixels_to_ascii_colored(frame)
 
+ASCII_CHARS = list("@#%WM&8B$O0QbdpqwmZOQLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
 
+
+width = 85   
+height = 50   
 
 
 
@@ -54,13 +50,6 @@ try:
     if len(sys.argv) < 2:
         raise AttributeError
     path = find(sys.argv[1], "./videos/")
-    print(path)
-
-    ASCII_CHARS = list(
-        "@#%WM&8B$O0QbdpqwmZOQLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
-
-    width = 85
-    height = 50
 
     ffmpeg_command = [
         "ffmpeg",
@@ -76,29 +65,36 @@ try:
     frame_height = 90
     frame_size = frame_width * frame_height * 3
 
-    process = subprocess.Popen(
-        ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    process = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+    def get_ascii_lines(ascii_frame):
+        return ascii_frame.splitlines()
+
+
+    sys.stdout.write("\033[2J")
+    sys.stdout.flush()
 
     while True:
         frame = process.stdout.read(frame_size)
-
         if len(frame) != frame_size:
             break
 
-        frame_np = np.frombuffer(frame, np.uint8).reshape(
-            (frame_height, frame_width, 3))
-
+        # Convertir los datos crudos a un array NumPy y luego a una imagen
+        frame_np = np.frombuffer(frame, np.uint8).reshape((frame_height, frame_width, 3))
         image = Image.fromarray(frame_np)
         ascii_frame = image_to_ascii(image)
+        
 
-   
+        sys.stdout.write("\033[H")
+
+        ascii_lines = get_ascii_lines(ascii_frame)
         sys.stdout.write(ascii_frame)
-        time.sleep(0.07)
-        sys.stdout.write("\033[H\033[J")
-       
+        sys.stdout.flush()
+        time.sleep(0.04)  
     process.stdout.close()
     process.stderr.close()
-    os.system('cls||clear')
     process.wait()
 except AttributeError:
     print("Hey insert something, don't expect the code to work for you like I did with ChatGPT")
